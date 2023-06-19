2ba024e2e848da34b944acac50173c64e9542066
Lucianor01 <lucianorosario02@gmail.com>
2023-06-20 00:02:31 +0200

    update route

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,16 @@ use App\Http\Controllers\Admin\ProjectController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
-Route::middleware('auth', 'verified')->prefix('admin')->namespace('admin.')->group(function () {
+Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/dashboard', [ProjectController::class, 'index'])->name('dashboard');
+    Route::get('/', [ProjectController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__ . '/auth.php';
