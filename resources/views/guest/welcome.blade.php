82de1d79e1fd624d835693c0ff5b02d44af47f67
Lucianor01 <lucianorosario02@gmail.com>
2023-06-21 16:56:48 +0200

    Link Project

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
 @section('content')
     <h1 class="text-center mt-3 text-uppercase">Portfolio di Luciano</h1>
-    <a href="{{ route('admin.project.index') }}">index</a>
 @endsection
